b5a791ebb680e58945a39fa657669e920c5d8c48
LLJ3288 <74459081+LLJ3288@users.noreply.github.com>
2021-02-16 11:39:38 -0600

    First commit

--- src/main/java/com/tts/techtalentblog/controller/BlogPostController.java
+++ src/main/java/com/tts/techtalentblog/controller/BlogPostController.java
@@ -3,12 +3,14 @@ package com.tts.techtalentblog.controller;
 import com.tts.techtalentblog.model.BlogPost;
 import com.tts.techtalentblog.repo.BlogPostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class BlogPostController {
@@ -27,6 +29,16 @@ public class BlogPostController {
         //returning a reference to that template
         //will allow us to show the data that we want
         model.addAttribute("posts", posts);
+
+
+            posts.removeAll(posts);                                     // added
+            for (BlogPost post : blogPostRepository.findAll()) {        // added
+                posts.add(post);                                        // added check curly brace if needed
+            }
+
+
+
+
         return "blogpost/index";
     }
 
@@ -44,18 +56,62 @@ public class BlogPostController {
         blogPostRepository.save(blogPost);
         //blogPost from our parameter is the object that we're getting from
         //the thymeleaf form, we can simply save it in our repository
+
+        posts.add(blogPost);
+
         model.addAttribute( "title", blogPost.getTitle());
         model.addAttribute( "author", blogPost.getAuthor());
         model.addAttribute( "blogEntry", blogPost.getBlogEntry());
         return "blogpost/result";
     }
 
+//    @RequestMapping(value = "/blogpost/{id}", method = RequestMethod.DELETE)
+//    public String deletePostWithId(@PathVariable Long id, BlogPost blogPost) {
+//
+//        blogPostRepository.deleteById(id);
+//        return "blogpost/index";
+
+//    }
+
+
     @RequestMapping(value = "/blogpost/{id}", method = RequestMethod.DELETE)
-    public String deletePostWithId(@PathVariable Long id, BlogPost blogPost) {
-
+    public String deletePostWithId(@PathVariable Long id, BlogPost blogPost, Model model) {
+        //crud repository method
         blogPostRepository.deleteById(id);
+        List<BlogPost> posts = blogPostRepository.findAll();
+        model.addAttribute("posts",posts);
         return "blogpost/index";
+    }
 
+
+
+
+    @RequestMapping(value = "/blogpost/{id}", method = RequestMethod.GET)
+    public String editPostWithId(@PathVariable Long id, BlogPost blogPost, Model model) {
+        Optional<BlogPost> post = blogPostRepository.findById(id);
+        if (post.isPresent()) {
+            BlogPost actualPost = post.get();
+            model.addAttribute("blogPost", actualPost);
+        }
+        return "blogpost/edit";
+    }
+
+
+
+    @RequestMapping(value = "/blogpost/update/{id}")
+
+    public String updateExistingPost(@PathVariable Long id, BlogPost blogPost, Model model) {
+        Optional<BlogPost> post = blogPostRepository.findById(id);
+        if (post.isPresent()) {
+            BlogPost actualPost = post.get();
+            actualPost.setTitle(blogPost.getTitle());
+            actualPost.setAuthor(blogPost.getAuthor());
+            actualPost.setBlogEntry(blogPost.getBlogEntry());
+            blogPostRepository.save(actualPost);
+            model.addAttribute("blogPost", actualPost);
+        }
+
+        return "blogpost/result";
     }
 
 
--- src/main/java/com/tts/techtalentblog/repo/BlogPostRepository.java
+++ src/main/java/com/tts/techtalentblog/repo/BlogPostRepository.java
@@ -4,9 +4,24 @@ import com.tts.techtalentblog.model.BlogPost;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 //we're passing the generic data types in to instruct CrudRepository how to setup our database
 public interface BlogPostRepository extends CrudRepository<BlogPost, Long> {
+
+
+
+    //added
+    List<BlogPost> findAll();
+    List<BlogPost> findByAuthorIgnoreCase(String author);
+	List<BlogPost> save(String blogPost);
+
+    BlogPost findById(long id);
+    BlogPost deleteById(long id);
+    // added
+
+
 
 }
